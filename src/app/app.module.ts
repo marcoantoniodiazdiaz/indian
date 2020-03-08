fe3f37e892ac7c2bdbc6a0b87e5e9b3b9011919c
Marco Antonio Diaz Diaz <kingmarkdc@gmail.com>
2020-03-08 08:45:47 -0600

    Primer Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ActivityComponent } from './pages/activity/activity.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ActivityComponent
   ],
   imports: [
     BrowserModule
